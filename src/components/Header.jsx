2187b74f0b3f1768d861eca73e8c45c56075f51c
Praveenraj5102 <praveenrajarumugam@gmail.com>
2025-03-13 11:35:42 +0530

    changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,18 @@ import Navbar from "./Navbar";
 import "./header.css";
 import "./Navbar.css";
 import { motion } from "motion/react"
+import { assets } from "../assets/assets";
 
 const Header = () => {
   return (
-    <div className="container-img" id="Header">
+    <div className="container-img min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"  id="Header">
       <Navbar />
       <motion.div
       initial={{opacity:0,y:100}}
       transition={{duration:1.5}}
       whileInView={{opacity:1,y:0}}
       viewport={{once:true}}
-      className="welcome">
+      className="welcome  ">
         <h2 className="header">Explore homes that fit your dreams</h2>
         <div className="space-x-6 mt-16">
           <a href="#Projects" className="border border-white px-8 py-3">Projects</a>
